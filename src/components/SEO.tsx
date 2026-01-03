c4a1e4d9e4f3f3d471f53dc6ac8793bfc29e8331
Adegboye Habeeb Temitope <115718654+Harbhite@users.noreply.github.com>
2026-01-04 00:15:58 +0100

    Update SEO.tsx

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -42,15 +42,14 @@ export const SEO = ({
     <Helmet>
       {/* Primary Meta Tags */}
       <title>{fullTitle}</title>
-      <meta name="title" content={fullTitle} />
       <meta name="description" content={truncatedDesc} />
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content={type} />
-      <meta property="og:url" content={pageUrl} />
-      <meta property="og:title" content={fullTitle} />
       <meta property="og:description" content={truncatedDesc} />
+            <meta property="og:type" content={type} />
       <meta property="og:image" content={imageUrl} />
+            <meta property="og:url" content={pageUrl} />
       <meta property="og:site_name" content="UISU Archive" />
 
       {/* Article specific */}
@@ -62,11 +61,13 @@ export const SEO = ({
       )}
 
       {/* X (Twitter) */}
+            <meta name="twitter:card" content="summary_large_image" />
+      
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={pageUrl} />
-      <meta property="twitter:title" content={fullTitle} />
-      <meta property="twitter:description" content={truncatedDesc} />
-      <meta property="twitter:image" content={imageUrl} />
+      <meta name="twitter:title" content={fullTitle} />
+      <meta name="twitter:description" content={truncatedDesc} />
+      <meta name="twitter:image" content={imageUrl} />
       <meta name="twitter:site" content="@uisu_archive" />
     </Helmet>
   );
